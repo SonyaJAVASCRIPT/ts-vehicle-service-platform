5d784a6711b92695f515f6c0c8d5fa30b9923503
SonyaJAVASCRIPT <ClaireRoush13iq@gmail.com>
2025-09-30 17:46:11 +0200

    feat(prisma): add prisma model

--- backend/user-service/src/app.controller.ts
+++ backend/user-service/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { AppService, type UserData } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Post()
-  async create(@Body() body: UserData) {
+  async postUsers(@Body() body: UserData) {
+    return this.appService.createUser(body);
+  }
+  @Get()
+  async getUsers(@Body() body: UserData) {
+    return this.appService.createUser(body);
+  }
+  @Get()
+  async getUserById(@Body() body: UserData) {
+    return this.appService.createUser(body);
+  }
+  @Put()
+  async putUserById(@Body() body: UserData) {
+    return this.appService.createUser(body);
+  }
+  @Delete()
+  async deleteUserById(@Body() body: UserData) {
     return this.appService.createUser(body);
   }
 }
--- backend/user-service/src/app.module.ts
+++ backend/user-service/src/app.module.ts
@@ -1,24 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'USER_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://guest:guest@localhost:5672'],
-          queue: 'user-queue',
-          queueOptions: {
-            durable: true,
-          },
-        },
-      },
-    ]),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
